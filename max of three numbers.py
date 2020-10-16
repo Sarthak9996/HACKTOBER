9835e33c05c7432888611eeacc64db6c2f4b6a32
Sarthak9996 <40289413+Sarthak9996@users.noreply.github.com>
2020-10-17 00:31:57 +0530

    Add files via upload

---
+++ max of three numbers.py
@@ -0,0 +1,7 @@
+x=int(input("Please enter the 1st value : "))
+y=int(input("Please enter the 2nd value : "))
+z=int(input("Please enter the 3rd value : "))
+
+
+
+input("Please press enter to exit")
