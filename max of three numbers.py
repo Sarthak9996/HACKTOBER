edd737b3f862d160150c59e630d468ff4859a8a6
pankajini31 <72996657+pankajini31@users.noreply.github.com>
2020-10-17 00:39:46 +0530

    Add files via upload

--- max of three numbers.py
+++ max of three numbers.py
@@ -2,6 +2,6 @@ x=int(input("Please enter the 1st value : "))
 y=int(input("Please enter the 2nd value : "))
 z=int(input("Please enter the 3rd value : "))
 
-
+print("Maximum value out of the given values :",max(x,y,z) )
 
 input("Please press enter to exit")
